bf8d8a71bce55d453f08ea084bfb59d97e7ee709
Omeiza Alabi <omeizaalabi@gmail.com>
2022-02-09 02:19:23 +0100

    changes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/portfolio-details', portfolioDetailsRouter);
 
 // Start Server
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
